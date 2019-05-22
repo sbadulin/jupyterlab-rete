6aec34c441cbf0b4f0945f5a06d36666d1b9ce29
Sergey Badulin <sbadulin@gmail.com>
2019-05-22 13:09:28 +0300

    Add empty widget

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import {
-  JupyterLab, JupyterLabPlugin
-} from '@jupyterlab/application';
+import { JupyterLab, JupyterLabPlugin } from '@jupyterlab/application';
+import { ICommandPalette } from '@jupyterlab/apputils';
+import { Widget } from '@phosphor/widgets';
 
 import '../style/index.css';
 
@@ -11,8 +11,31 @@ import '../style/index.css';
 const extension: JupyterLabPlugin<void> = {
   id: 'jupyter-plugin-test',
   autoStart: true,
-  activate: (app: JupyterLab) => {
+  requires: [ICommandPalette],
+  activate: (app: JupyterLab, palette: ICommandPalette) => {
     console.log('JupyterLab extension jupyter-plugin-test is activated!');
+
+    const widget: Widget = new Widget();
+    widget.id = 'jupyter-plugin-test';
+    widget.title.label = 'Vega 2';
+    widget.title.closable = true;
+
+    // Add an application command
+    const command: string = 'vega:open';
+    app.commands.addCommand(command, {
+      label: 'Vega 2 with Rete.js',
+      execute: () => {
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not there
+          app.shell.addToMainArea(widget);
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    });
+
+    // Add the command to the palette
+    palette.addItem({command, category: 'App'});
   }
 };
 
